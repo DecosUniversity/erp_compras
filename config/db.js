208a8faf3311131c61d31cc827356e09f1859fa0
DennisC <dcornels@miumg.edu.gt>
2025-10-06 21:26:35 -0600

    nuevo despliegue waitConections

--- config/db.js
+++ config/db.js
@@ -9,11 +9,14 @@ console.log({
 });
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQLHOST || process.env.DB_HOST,
-  user: process.env.MYSQLUSER || process.env.DB_USER,
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-  port: process.env.MYSQLPORT || process.env.DB_PORT
+  host: process.env.MYSQLHOST || process.env.DB_HOST || 'localhost',
+  user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || '',
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME || 'analisis_dos',
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  queueLimit: 0,
+  connectTimeout: 10000
 });
 
 module.exports = pool.promise(); // Para usar async/await
