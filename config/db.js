d6e3c8255198284e9e99c5bafda7592d43a5328e
DennisC <dcornels@miumg.edu.gt>
2025-10-06 19:43:08 -0600

    nuevo despliegue 2

--- config/db.js
+++ config/db.js
@@ -9,11 +9,11 @@ console.log({
 });
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQLHOST || process.env.DB_HOST || 'localhost',
-  user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || '',
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME || 'analisis_dos',
-  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: process.env.MYSQLPORT || process.env.DB_PORT
 });
 
 module.exports = pool.promise(); // Para usar async/await
